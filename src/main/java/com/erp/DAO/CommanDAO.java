0dc5275a2d1563feb7dd7652a9ec6bc1220fe157
Sampadak-04 <kothari.sampada33@gmail.com>
2022-04-16 20:05:07 +0530

    Wrote code for add faculty

--- src/main/java/com/erp/DAO/CommanDAO.java
+++ src/main/java/com/erp/DAO/CommanDAO.java
@@ -13,15 +13,17 @@ public class CommanDAO {
 	public int Add_faculty(String fname,String mname,String lname,String email,String education,String password)
 	{
 		int i=0;
-		
+		con = DBConnection.getConnection();
 			try {
-				ps=con.prepareStatement("insert into erp_faculty fisrtname,middlename,lastname,email,education,password, values(?,?,?,?,?,?)");
+				ps=con.prepareStatement("insert into erp_faculty (firstname,middlename,lastname,email,education,password) values(?,?,?,?,?,?)");
 				ps.setString(1,fname);
 				ps.setString(2, mname);
 				ps.setString(3,lname);
 				ps.setString(4,email);
 				ps.setString(5,education);
 				ps.setString(6, password);
+				i = ps.executeUpdate();
+				
 				
 			} catch (SQLException e) {
 				// TODO Auto-generated catch block
